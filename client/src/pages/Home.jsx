8ede5dff154b167101d4dfba5af5d351d20a1ede
Sanjay <sanjayrydv@gmail.com>
2023-06-29 19:57:35 +0530

    Loading picture on cloudinary

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -20,6 +20,32 @@ const Home = () => {
   const [allPosts, setAllPosts] = useState(null);
   const [searchText, setSearchText] = useState('');
 
+  const fetchPosts = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch('http://localhost:8080/api/v1/post', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        setAllPosts(result.data.reverse());
+      }
+    } catch (err) {
+      alert(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
 
   return (
     <section className='max-w-7xl mx-auto'>
@@ -51,12 +77,12 @@ const Home = () => {
             <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
               {searchText ? (
                 <RenderCards 
-                data={[]} 
+                data={allPosts} 
                 title="No search results found!"
                 />
               ):(
                 <RenderCards 
-                data={[]}
+                data={allPosts}
                 title="No posts found!"
                 />
               )}
